57e84aedc97f51c2b87e2c75b78cd88e19ab178c
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-06-04 23:34:38 +0530

    learned components and props;

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar=(props)=>{
+    return 
+    <>
+    <div className="navbar">
+        <div className="logo-contain">
+            <img src="https://i.ibb.co/RNSJXNC/FB-IMG-1652607151685.jpg" alt="logo" className="nav-logo" />
+            <p className="logo-title">props.logo_title</p>
+        </div>
+        <ul className="nav-items">
+            <li className="nav-lists"><a href="/" className="nav-link">Projects</a></li>
+            <li className="nav-lists"><a href="/" className="nav-link">Blogs</a></li>
+            <li className="nav-lists"><a href="/" className="nav-link">Skills</a></li>
+            <li className="nav-lists"><a href="/" className="nav-link">Gears</a></li>
+            <li className="nav-lists"><a href="/" className="nav-link">Gallery</a></li>
+        </ul>
+        <ul className="social">
+            <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-facebook-circle'></i></a></li>
+            <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-instagram-alt'></i></a></li>
+            <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-telegram'></i></a></li>
+            <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-github'></i></a></li>
+            <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-twitter'></i></a></li>
+        </ul>
+    </div>
+    </>
+
+}
