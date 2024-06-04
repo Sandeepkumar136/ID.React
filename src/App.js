092da3759400a8291fd9e67f31e3332d453bd9f4
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-06-04 23:35:27 +0530

    What changed i don't know

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import './App.css';
-import { author } from './Script.mjs';
-
-
+import './style.css';
+import Navigation from './components/Navbar';
 function App() {
   return (
-    <div className='App'>
-      <nav className="navbar">
-        <div className="logo-containe">
-          <img src="https://i.ibb.co/D7TGdLS/FB-IMG-1652607151685.jpg" alt="logo" className="nav-logo" />
-          <p className="nav-tittle">{author}</p>
-        </div>
-        <ul className="nav-items">
-          <li className="nav-lists"><a href="/" className="nav-links">Projects</a></li>
-          <li className="nav-lists"><a href="/" className="nav-links">Blogs</a></li>
-          <li className="nav-lists"><a href="/" className="nav-links">Skills</a></li>
-          <li className="nav-lists"><a href="/" className="nav-links">Gears</a></li>
-          <li className="nav-lists"><a href="/" className="nav-links">Gallery</a></li>
-        </ul>
-        <ul className="social-items">
-          <li className="social-lists"><a href="/" className="social-links bx bxl-facebook-circle"></a></li>
-          <li className="social-lists"><a href="/" className="social-links bx bxl-instagram-alt"></a></li>
-          <li className="social-lists"><a href="/" className="social-links bx bxl-telegram"></a></li>
-          <li className="social-lists"><a href="/" className="social-links bx bxl-github"></a></li>
-          <li className="social-lists"><a href="/" className="social-links bx bxs-moon"></a></li>
-        </ul>
-      </nav>
-    </div>
+    <>
+    <Navigation logo_title="Sandeep Kumar"/>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const Navbar=(props)=>{
-    return 
-    <>
-    <div className="navbar">
+ function Navigation(props){
+    return(
+        <div className="navbar">
         <div className="logo-contain">
             <img src="https://i.ibb.co/RNSJXNC/FB-IMG-1652607151685.jpg" alt="logo" className="nav-logo" />
-            <p className="logo-title">props.logo_title</p>
+            <p className="logo-title">{props.logo_title}</p>
         </div>
         <ul className="nav-items">
             <li className="nav-lists"><a href="/" className="nav-link">Projects</a></li>
@@ -20,9 +19,14 @@ const Navbar=(props)=>{
             <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-instagram-alt'></i></a></li>
             <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-telegram'></i></a></li>
             <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-github'></i></a></li>
-            <li className="social-list"><a href="/" className="social-links"><i className='bx bxl-twitter'></i></a></li>
+            <li className="social-list"><a href="/" className="social-links"><i className='bx bxs-moon' ></i></a></li>
         </ul>
+        <div className="toggle-f-nav">
+        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-x"></i>
+        </div>
     </div>
-    </>
+    )
 
 }
+export default Navigation;
