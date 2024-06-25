005a446a4cc62f91595414395411f2e96d08d152
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-06-25 22:59:04 +0530

    Dstructured Json

--- src/components/BlogPaginator.js
+++ src/components/BlogPaginator.js
@@ -1,56 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from 'react'
 
 export default function BlogPaginator() {
-  const [blogs, Setblogs] = useState([]);
-  const [currentPage, setCurrentPage] = useState(6);
-  useEffect(() => {
-    const FetchBlogs = async () => {
-      try {
-        const response = await fetch("BlogData.json");
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        Setblogs(data);
-      } catch (error) {
-        console.error("Error fetching Blogs:", error);
-      }
-    };
-    FetchBlogs();
-  }, []);
-
-  const LoadMore = () => {
-    setCurrentPage((pervCount) => pervCount + 6);
-    document
-      .querySelector(".blog-container")
-      .scrollIntoView({ behavior: "smooth" });
-  };
   return (
     <div>
-      <div className="blog-container">
-        {blogs.slice(0, currentPage).map((element) => (
-          <div key={element.id} className="Blogcard">
-            <div className="bc-up-contain">
-              <img src={element.img} alt="BlogLogo" className="logo-b" />
-              <div className="title-b">{element.username}</div>
-              <div className="b-dep-date">{element.date}</div>
-            </div>
-            <div className="b-base-contain">
-              <div className="b-base-title">{element.title}</div>
-              <div className="b-b-disc">{element.description}</div>
-              <div className="b-b-base-link">
-                <a href={element.link} className="b-b-link">
-                  view in blogspot
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      {currentPage < blogs.length && (
-        <button onClick={LoadMore}>LoadMore</button>
-      )}
-      ;
+      
     </div>
-  );
+  )
 }
---
+++ src/components/skillsrsl.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/sRcPs7j/Backend-2.jpg",
+        disc: "Frontend development involves creating user interfaces and experiences, focusing on design, responsiveness, and performance."
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/1ZB90ZR/Backend-1.jpg",
+        disc: "Backend development involves server-side logic, database management, and ensuring the smooth operation of applications."
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/wLY6jrW/Untitled-design.jpg",
+        disc: "A freelancer works independently, offering specialized services on a project-by-project basis to various clients."
+    }
+];
